2c35d086f8ed34baf6de5cd51eb082358f907a90
eddie <eddie@eddies-MacBook-Pro.local>
2024-08-13 21:54:27 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
+    base: '/',
     plugins: [
         createHtmlPlugin({
             minify: true,
